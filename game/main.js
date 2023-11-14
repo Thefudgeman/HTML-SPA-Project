3bc1d4fd94d170df15abf4f220ef63720949b747
Thefudgeman <146274190+Thefudgeman@users.noreply.github.com>
2023-11-14 17:20:02 +0000

    map

--- game/main.js
+++ game/main.js
@@ -1,40 +1,35 @@
 import './style.css'
 import Phaser from 'phaser'
 
-const Sizes={
-  width:500,
-  height:500
+const Sizes = {
+    width: 500,
+    height: 500
 }
 
 const speedDown = 300
 
-class GameScene extends Phaser.Scene
-{
-  constructor()
-  {
-    super("scene-game")
-  }
+class GameScene extends Phaser.Scene {
+    constructor() {
+        super("scene-game")
+    }
 
-  preload()
-  {
-    this.load.image("map", "assets/2D Pixel Dungeon Asset Pack/character and tileset/demonstration.png")
-  }
+    preload() {
+        this.load.image("map", "assets/map.png")
+    }
 
-  create()
-  {
-    this.add.image(0,0, "map").setOrigin(0,0)
-  }
+    create() {
+        this.add.image(0, 0, "map").setOrigin(0, 0)
+    }
 
-  update()
-  {}
+    update() {}
 }
 
 const config = {
-  type:Phaser.WEBGL,
-  width:Sizes.width,
-  height:Sizes.height,
-  canvas:gameCanvas,
-  scene:[GameScene]
+    type: Phaser.WEBGL,
+    width: Sizes.width,
+    height: Sizes.height,
+    canvas: gameCanvas,
+    scene: [GameScene]
 }
 
 const game = new Phaser.Game(config)
