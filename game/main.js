81496abee1f9f6eb9542611a85ef2703c10cd5f7
Thefudgeman <146274190+Thefudgeman@users.noreply.github.com>
2023-11-15 08:55:19 +0000

    add player image

--- game/main.js
+++ game/main.js
@@ -2,23 +2,25 @@ import './style.css'
 import Phaser from 'phaser'
 
 const Sizes = {
-    width: 500,
-    height: 500
+    width: 960,
+    height: 640
 }
 
-const speedDown = 300
 
 class GameScene extends Phaser.Scene {
     constructor() {
         super("scene-game")
+        this.player
     }
 
     preload() {
         this.load.image("map", "assets/map.png")
+        this.load.image("player", "assets/sprites/player.png")
     }
 
     create() {
         this.add.image(0, 0, "map").setOrigin(0, 0)
+        this.plaer = this.physics.add.image(0,0, "player").setOrigin(400, 300)
     }
 
     update() {}
@@ -29,6 +31,15 @@ const config = {
     width: Sizes.width,
     height: Sizes.height,
     canvas: gameCanvas,
+    physics:
+    {
+        default:"arcade",
+        arcade:
+        {
+            gravity:{y:0},
+            debug:false
+        }
+    },
     scene: [GameScene]
 }
 
