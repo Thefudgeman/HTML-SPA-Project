81b6f5acbd46eeca457cd8a5f27e9d7cf8f253dc
Thefudgeman <146274190+Thefudgeman@users.noreply.github.com>
2023-11-20 11:51:56 +0000

    edit player JSON file

--- game/main.js
+++ game/main.js
@@ -1,5 +1,8 @@
 import './style.css'
 import Phaser from 'phaser'
+let spriteData;
+let animation = [];
+let playerC;
 
 const Sizes = {
     width: 960,
@@ -15,12 +18,30 @@ class GameScene extends Phaser.Scene {
 
     preload() {
         this.load.image("map", "assets/map.png")
-        this.load.image("player", "assets/sprites/characters/player.png")
+        this.load.spritesheet("player", "assets/sprites/characters/player.png")
+        frameWidth:48
+        frameHeight:48
     }
+
+    setup()
+    {
+        let frames = spriteData.frames  
+        for(let i = 0; i <frames.length; i++) 
+        {
+            let pos = frames[i].position
+            let img = spriteSheet.get(pos.x, pos.y, pos.w, pos.h)
+            animation[i] = img
+        }
+            playerC = new sprite(animation, 1)
+    }
+
 
     create() {
         this.add.image(0, 0, "map").setOrigin(0, 0)
-        this.player = this.physics.add.image(0,0, "player").setOrigin(0,0)
+        //this.player = this.physics.add.image(0,0, "player").setOrigin(0,0)
+        this.add.image(0,0, animation[10])
+        playerC.animate()
+        playerC.show()
     }
 
     update() {}
@@ -40,7 +61,7 @@ const config = {
             debug:false
         }
     },
+
     scene: [GameScene]
 }
-
 const game = new Phaser.Game(config)
