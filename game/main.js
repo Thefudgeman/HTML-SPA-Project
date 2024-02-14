f4403713fd9551eabc82191822d72b3907af5786
Thefudgeman <146274190+Thefudgeman@users.noreply.github.com>
2024-02-14 10:43:30 +0000

    shop buttons

--- game/main.js
+++ game/main.js
@@ -30,12 +30,33 @@ class shopScene extends Phaser.Scene{
     }
     preload()
     {
-
+        this.load.image("shop", "src/assets/2D Pixel Dungeon Asset Pack/character and tileset/demonstration.png")
     }
 
     create()
     {
+        this.add.image(0,0,"shop").setOrigin(0,0)
 
+        let BuyButton = this.add.text(230,300,"Buy").setInteractive().on('pointerdown', () => this.BuyButtonClicked())
+        let SellButton = this.add.text(430,300,"Sell").setInteractive().on('pointerdown', () => this.SellButtonClicked())
+        let LeaveButton = this.add.text(630,300,"Leave").setInteractive().on('pointerdown', () => this.LeaveButtonClicked())
+
+
+    }
+
+    BuyButtonClicked()
+    {   
+
+    }
+
+    SellButtonClicked()
+    {
+
+    }
+
+    LeaveButtonClicked()
+    {
+        
     }
 
     update()
